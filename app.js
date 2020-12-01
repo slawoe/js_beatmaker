0d5b9bce9ee66ae74b69cd49a11146d364f19502
Slawomir Ernst <info@slawomirernst.de>
2020-12-01 15:13:02 +0100

    Add active class if pad is clicked as toggle

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ class DrumKit {
     this.index = 0;
     this.bpm = 150;
   }
+  activePad() {
+    this.classList.toggle("active");
+  }
   repeat() {
     let step = this.index % 8;
     const activeBars = document.querySelectorAll(`.b${step}`);
@@ -23,6 +26,9 @@ class DrumKit {
 }
 
 const drumKit = new DrumKit();
+drumKit.pads.forEach((pad) => {
+  pad.addEventListener("click", drumKit.activePad);
+});
 drumKit.playBtn.addEventListener("click", function () {
   drumKit.start();
 });
