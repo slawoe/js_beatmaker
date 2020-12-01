80f322ab999b53ca2905b62f9c435b574d157958
Slawomir Ernst <info@slawomirernst.de>
2020-12-01 14:39:56 +0100

    Create constructor for pad, kick, snare and hihat

---
+++ app.js
@@ -0,0 +1,8 @@
+class DrumKit {
+  constructor() {
+    this.pads = document.querySelectorAll(".pad");
+    this.kickAudio = document.querySelector(".kick-sound");
+    this.snareAudio = document.querySelector(".snare-sound");
+    this.hihatAudio = document.querySelector(".hihat-sound");
+  }
+}
