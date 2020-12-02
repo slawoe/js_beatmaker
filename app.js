dd4c27e5feef99c3cbb7272a6901692f8fb83e1d
Slawomir Ernst <info@slawomirernst.de>
2020-12-02 17:24:07 +0100

    Change animation in drumKit.pads so animation repeats over and over again

--- app.js
+++ app.js
@@ -30,6 +30,9 @@ class DrumKit {
 const drumKit = new DrumKit();
 drumKit.pads.forEach((pad) => {
   pad.addEventListener("click", drumKit.activePad);
+  pad.addEventListener("animationend", function () {
+    this.style.animation = "";
+  });
 });
 drumKit.playBtn.addEventListener("click", function () {
   drumKit.start();
