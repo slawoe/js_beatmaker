5bb76401a9f800b30465a6c5482d4efb4b42fe0d
Slawomir Ernst <info@slawomirernst.de>
2020-12-07 10:21:38 +0100

    Move bpm from changeTempo to updateTempo

--- app.js
+++ app.js
@@ -101,8 +101,16 @@ class DrumKit {
   }
   changeTempo(e) {
     const tempoText = document.querySelector(".tempo-nr");
-    this.bpm = e.target.value;
     tempoText.innerText = e.target.value;
+  }
+  updateTempo(e) {
+    this.bpm = e.target.value;
+    clearInterval(this.isPlaying);
+    this.isPlaying = null;
+    const playBtn = document.querySelector("#play");
+    if (playBtn.classList.contains("active")) {
+      this.start();
+    }
   }
 }
 
@@ -128,4 +136,7 @@ drumKit.muteButtons.forEach((btn) => {
 });
 drumKit.tempoSlider.addEventListener("input", function (e) {
   drumKit.changeTempo(e);
+});
+drumKit.tempoSlider.addEventListener("change", function (e) {
+  drumKit.updateTempo(e);
 });
